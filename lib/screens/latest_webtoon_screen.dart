7f0075218d75ca6407ae9ddd162159c3107cb056
KangMins <rkdalsdl98@naver.com>
2023-06-13 11:08:41 +0900

    '웹툰'

--- lib/screens/latest_webtoon_screen.dart
+++ lib/screens/latest_webtoon_screen.dart
@@ -27,13 +27,14 @@ class _LatestWebtoonScreenState extends State<LatestWebtoonScreen> {
   double _currPageValue = 0.0;
 
   final double _scaleFactor = 0.8;
-  late WebtoonModel _currWebtoon;
+  late WebtoonModel? _currWebtoon;
 
   @override
   void initState() {
     super.initState();
+    var webtoons = Get.find<UserController>().likeWebtoons;
     _currWebtoon =
-        Get.find<UserController>().likeWebtoons[pageController.initialPage];
+        webtoons.isNotEmpty ? webtoons[pageController.initialPage] : null;
     pageController.addListener(() {
       setState(() {
         _currPageValue = pageController.page!;
@@ -51,7 +52,15 @@ class _LatestWebtoonScreenState extends State<LatestWebtoonScreen> {
           image: DecorationImage(
             opacity: .1,
             fit: BoxFit.cover,
-            image: NetworkImage(_currWebtoon.thumb),
+            image: NetworkImage(
+              _currWebtoon != null
+                  ? _currWebtoon!.thumb
+                  : 'https://image-comic.pstatic.net/webtoon/774044/thumbnail/thumbnail_IMAG21_81504afb-1a05-41b0-9650-0c9aa1d741d9.jpg',
+              headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+              },
+            ),
           ),
         ),
         child: Padding(
@@ -93,7 +102,14 @@ class _LatestWebtoonScreenState extends State<LatestWebtoonScreen> {
                     child: GetBuilder<UserController>(builder: (controller) {
                       final data = controller.likeWebtoons;
                       if (data.isEmpty) {
-                        return const Center(child: CircularProgressIndicator());
+                        return const Center(
+                            child: Text(
+                          '관심을 표시한 웹툰이 없습니다.',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ));
                       }
                       return PageView.builder(
                         itemCount: data.length,
--- lib/widgets/latest_webtoon/latest_webtoon_colum.dart
+++ lib/widgets/latest_webtoon/latest_webtoon_colum.dart
@@ -4,7 +4,7 @@ import 'package:toonflix/service/models/WebtoonModel.dart';
 import '../../globalfuncs/Desigh.dart';
 
 class LatestWebtoonColum extends StatelessWidget {
-  final WebtoonModel webtoon;
+  final WebtoonModel? webtoon;
 
   const LatestWebtoonColum({
     super.key,
@@ -15,36 +15,38 @@ class LatestWebtoonColum extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        RichText(
-          text: TextSpan(
-            text: '${webtoon.company.toUpperCase()} ',
-            style: TextStyle(
-              color: identifierColor[webtoon.company],
-              fontSize: 20,
-              fontWeight: FontWeight.w900,
-            ),
-            children: const [
-              TextSpan(
-                text: '웹툰',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w900,
-                ),
+        if (webtoon != null)
+          RichText(
+            text: TextSpan(
+              text: '${webtoon!.company.toUpperCase()} ',
+              style: TextStyle(
+                color: identifierColor[webtoon!.company],
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
               ),
-            ],
+              children: const [
+                TextSpan(
+                  text: '웹툰',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
         SizedBox(
           height: 10 * scaleHeight(context),
         ),
-        Text(
-          webtoon.title,
-          style: const TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.w600,
+        if (webtoon != null)
+          Text(
+            webtoon!.title,
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
           ),
-        ),
       ],
     );
   }
